7602d24bad22344bfddb5e8bc8c93fa42b3581cc
Ludal <44783088+iamludal@users.noreply.github.com>
2021-02-23 00:17:25 +0100

    refactor: ♻️ Use article instead of div for Article container

--- components/Article.jsx
+++ components/Article.jsx
@@ -6,11 +6,11 @@ const Article = ({ title, content, date, ...props }) => {
   const dateStr = strftime('%d/%m/%Y', new Date(date))
 
   return (
-    <div className={styles.article} {...props}>
+    <article className={styles.article} {...props}>
       <h2>{title}</h2>
       <small>Posté le {dateStr} </small>
       <p dangerouslySetInnerHTML={{ __html: content }}></p>
-    </div>
+    </article>
   )
 }
 
