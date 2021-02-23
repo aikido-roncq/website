955284c9579b162452bf0787e6e83e9303e36d51
Ludal <44783088+iamludal@users.noreply.github.com>
2021-02-24 00:06:57 +0100

    refactor: ✨ Update homepage (add Articles, Events, Schedules, Links)

---
+++ components/Event.jsx
@@ -0,0 +1,41 @@
+import { useCallback, useState } from 'react'
+import strftime from 'strftime'
+import Button from './inputs/Button'
+import styles from '../styles/components/Event.module.scss'
+
+const DATE_FORMAT = '%d %b'
+
+const makeDateStr = (startStr, endStr) => {
+  const start = new Date(startStr)
+  const end = new Date(endStr)
+
+  if (!endStr)
+    return strftime(DATE_FORMAT, start)
+  else if (start.getTime() == end.getTime())
+    return makeDateStr(start)
+  else if (start.getMonth() == end.getMonth())
+    return strftime('%d', start) + '-' + makeDateStr(endStr)
+
+  return makeDateStr(start) + ' - ' + makeDateStr(endStr)
+}
+
+const Event = ({ title, info, start_date, end_date }) => {
+
+  const makeDateMemo = useCallback(makeDateStr, [start_date, end_date])
+  const [hidden, setHidden] = useState(true)
+
+  const toggleHidden = () => setHidden(h => !h)
+
+  return (
+    <div className={styles.container}>
+      <strong>{makeDateMemo(start_date, end_date)}</strong>
+      <span>{title}</span>
+      <Button onClick={toggleHidden} title="Plus d'infos">+</Button>
+      <div className={styles.info} hidden={hidden}>
+        {info}
+      </div>
+    </div>
+  )
+}
+
+export default Event
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,28 @@
 import { useEffect, useState } from 'react'
+import { FaFacebook, FaInstagram } from 'react-icons/fa'
+import Article from '../components/Article'
+import Event from '../components/Event'
+import Link from '../components/Link'
+import Schedules from '../components/Schedules'
 import Head from 'next/head'
+import axios from 'axios'
 
-const { API_URL } = process.env
+const API_URL = process.env.API_URL
+const FACEBOOK = process.env.FACEBOOK
+const INSTAGRAM = process.env.INSTAGRAM
 
 export default function Home() {
 
   const [articles, setArticles] = useState([])
-  const [error, setError] = useState('')
+  const [events, setEvents] = useState([])
 
   useEffect(() => {
-    fetch(API_URL + '/articles')
-      .then(res => res.json())
-      .then(setArticles)
-      .catch(e => setError(e.message))
+    axios.get(API_URL + '/articles')
+      .then(res => setArticles(res.data))
+      .catch(e => console.log(e))
+    axios.get(API_URL + '/events')
+      .then(res => setEvents(res.data))
+      .catch(e => console.log(e))
   }, [])
 
   return (
@@ -20,10 +30,37 @@ export default function Home() {
       <Head>
         <title>Accueil | Aïkido Roncq</title>
       </Head>
-      <h1>Hello</h1>
-      {error && <p style={{ color: 'red' }}>{error}</p>}
-      {articles && <p>There are {articles.length} articles</p>}
-      {articles.map(a => <div key={a.slug}>{a.content}</div>)}
+      <div className="container">
+        <div className="articles">
+          <h1>📝 Derniers articles</h1>
+          {articles.length ? (
+            articles.map(a => <Article key={a.slug} {...a} />)
+          ) : (
+              <span>Aucun article récent.</span>
+            )}
+        </div>
+        <div className="social-networks">
+          <h1>👥 Réseaux sociaux</h1>
+          <div className="network">
+            <FaFacebook /> <Link href={FACEBOOK}>Facebook</Link>
+          </div>
+          <div className="network">
+            <FaInstagram /> <Link href={INSTAGRAM}>Instagram</Link>
+          </div>
+        </div>
+        <div className="schedules">
+          <h1>🕙 Horaires</h1>
+          <Schedules />
+        </div>
+        <div className="events">
+          <h1>📅 Évènements</h1>
+          {events.length ? (
+            events.map(event => <Event key={event.id} {...event} />)
+          ) : (
+              <span>Aucun événement à venir.</span>
+            )}
+        </div>
+      </div>
     </>
   )
 }
