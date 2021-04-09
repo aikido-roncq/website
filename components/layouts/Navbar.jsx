238208e3aad49aabbd60af9283cc0dd302ab4bed
Ludal <44783088+iamludal@users.noreply.github.com>
2021-04-09 15:41:18 +0200

    refactor: ♻️ Navbar routes

--- components/layouts/Navbar.jsx
+++ components/layouts/Navbar.jsx
@@ -1,29 +1,41 @@
 import Link from 'components/Link'
 import { useRouter } from 'next/router'
-import { useRef } from 'react'
 import styles from 'styles/components/Navbar.module.scss'
+
+const ROUTES = [
+  {
+    path: '/',
+    label: 'Accueil',
+  },
+  {
+    path: '/aikido',
+    label: "Découvrir l'Aïkido",
+  },
+  {
+    path: '/aikishintaiso',
+    label: "Découvrir l'Aïkishintaïso",
+  },
+  {
+    path: '/articles',
+    label: 'Articles',
+  },
+  {
+    path: '/rejoindre',
+    label: 'Nous Rejoindre',
+  },
+]
 
 const Navbar = () => {
   const router = useRouter()
-
-  const routes = useRef({
-    '/': 'Accueil',
-    '/aikido': "Découvrir l'Aïkido",
-    '/aikishintaiso': "Découvrir l'Aïkishintaïso",
-    '/articles': 'Articles',
-    '/rejoindre': 'Nous rejoindre',
-  })
-
   const getClassName = (route) => (isActive(route) ? styles.active : '')
-
   const isActive = (route) => route == router.pathname
 
   return (
     <nav className={styles.nav}>
       <ul>
-        {Object.entries(routes.current).map(([route, text]) => (
-          <li className={getClassName(route)} key={route}>
-            <Link href={route}>{text}</Link>
+        {ROUTES.map(({ path, label }) => (
+          <li className={getClassName(path)} key={path}>
+            <Link href={path}>{label}</Link>
           </li>
         ))}
       </ul>
