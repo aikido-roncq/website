bf7fcd5b16afa0179f070909eb7e6c51d2bc2513
Ludo <ludovic.chombeau@leroymerlin.fr>
2021-04-30 20:40:55 +0200

    fix(HomePage): right panel width

--- components/Event.jsx
+++ components/Event.jsx
@@ -13,7 +13,7 @@ const Event = ({ title, info, start_date, end_date }) => {
         <span title="Plus d'infos" className={styles.button}>
           {hidden ? '+' : '-'}
         </span>
-        <span className={styles.title}>{title}</span>•
+        <span className={styles.title}>{title}</span>
         <span className={styles.date}>{dateRangeStr}</span>
       </div>
       <div className={styles.info} hidden={hidden}>
