7d4bf043f4322fe93df0248dfec2d9b7357c1669
Ludal <ludovic.chombeau@gmail.com>
2021-05-20 23:17:51 +0200

    feat(Button): add loading spinner

--- components/inputs/Button.jsx
+++ components/inputs/Button.jsx
@@ -1,8 +1,9 @@
 import styles from '@/styles/components/inputs/Button.module.scss'
 
-const Button = ({ children, ...props }) => {
+const Button = ({ children, icon, loading = false, ...props }) => {
   return (
-    <button className={styles.btn} {...props}>
+    <button className={styles.btn} disabled={loading} {...props}>
+      {loading ? <div className={styles.loader}></div> : icon}
       {children}
     </button>
   )
