c12f88ea34c85793620f034f518582dcc44c1fbd
Ludal <ludovic.chombeau@gmail.com>
2021-06-22 20:28:25 +0200

    feat(dashboard): show home icon on mobile in header

--- components/dashboard/Header.jsx
+++ components/dashboard/Header.jsx
@@ -1,18 +1,33 @@
-import { Grid, Button, Heading } from '@chakra-ui/react'
+import { Grid, Button, Heading, useBreakpointValue, IconButton } from '@chakra-ui/react'
 import { ChevronLeftIcon } from '@chakra-ui/icons'
 import Link from '@/components/Link'
+import { TiHome } from 'react-icons/ti'
 
-const Header = () => (
-  <Grid templateColumns="1fr auto 1fr" alignItems="center" justifyItems="start" mb={4}>
-    <Link href="/">
-      <Button variant="ghost">
-        <ChevronLeftIcon /> Page d'accueil
-      </Button>
-    </Link>
-    <Heading as="h1" size="xl" my={4}>
-      Tableau de bord
-    </Heading>
-  </Grid>
-)
+const Header = () => {
+  const isDesktop = useBreakpointValue({ base: false, md: true })
+
+  return (
+    <Grid
+      templateColumns="1fr auto 1fr"
+      alignItems="center"
+      justifyItems="start"
+      mb={4}
+      gap={4}
+    >
+      <Link href="/">
+        {isDesktop ? (
+          <Button variant="ghost">
+            <ChevronLeftIcon /> Page d'accueil
+          </Button>
+        ) : (
+          <IconButton icon={<TiHome />} />
+        )}
+      </Link>
+      <Heading as="h1" size="xl" my={4}>
+        Tableau de bord
+      </Heading>
+    </Grid>
+  )
+}
 
 export default Header
