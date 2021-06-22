d834932e725118c3eeae856582efd52f64cd28f2
Ludal <ludovic.chombeau@gmail.com>
2021-06-22 20:31:38 +0200

    feat(dashboard): add gaps between actions

--- components/dashboard/Action.jsx
+++ components/dashboard/Action.jsx
@@ -2,7 +2,7 @@ import { Tooltip, Button } from '@chakra-ui/react'
 
 const Action = ({ label, icon, onClick, isLast, color }) => (
   <Tooltip hasArrow label={label}>
-    <Button onClick={onClick} mr={isLast ? 0 : 3} colorScheme={color}>
+    <Button onClick={onClick} mr={isLast ? 0 : 3} my={2} colorScheme={color}>
       {icon}
     </Button>
   </Tooltip>
