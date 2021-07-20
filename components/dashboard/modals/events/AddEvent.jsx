e40ca7e6b72b3f3f2130c68c33db4c10de3ac080
Ludal <ludovic.chombeau@gmail.com>
2021-07-20 19:33:56 +0200

    refactor: update AddEvent modal

--- components/dashboard/modals/events/AddEvent.jsx
+++ components/dashboard/modals/events/AddEvent.jsx
@@ -1,44 +1,26 @@
-import { ModalBody, ModalHeader, ModalFooter } from '@chakra-ui/modal'
-import {
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Textarea,
-  useBoolean,
-} from '@chakra-ui/react'
+import { ModalBody, ModalFooter, ModalHeader } from '@chakra-ui/modal'
+import { Button, FormControl, FormLabel, Input, Textarea, useBoolean } from '@chakra-ui/react'
 import Modal from '@/components/Modal'
 import strftime from 'strftime'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useForm } from 'react-hook-form'
 
 const DATE_FORMAT = '%Y-%m-%d'
 
 const AddEvent = ({ isOpen, onClose, submitCallback, edit, event }) => {
   const [submitting, setSubmitting] = useBoolean(false)
-  const [data, setData] = useState(edit ? event : {})
+  const { register, reset, handleSubmit, watch } = useForm(event || {})
+  const watching = watch('start_date', strftime(DATE_FORMAT))
 
-  const handleSubmit = async () => {
+  console.log(watching)
+
+  const onSubmit = async (data) => {
     setSubmitting.on()
-
     const posted = await submitCallback(data)
-
     if (posted) {
-      setData({})
+      reset()
     }
-
     setSubmitting.off()
   }
-
-  const handleInputChange = (input) => {
-    return (e) => {
-      setData((oldData) => ({ ...oldData, [input]: e.target.value }))
-    }
-  }
-
-  useEffect(() => {
-    setData(edit ? event : {})
-  }, [edit, event])
 
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
@@ -47,46 +29,36 @@ const AddEvent = ({ isOpen, onClose, submitCallback, edit, event }) => {
         <FormControl mb={4}>
           <FormLabel>Titre</FormLabel>
           <Input
-            value={data.title || ''}
-            onChange={handleInputChange('title')}
+            name="title"
             placeholder="Stage à la mer, passage de grades..."
+            ref={register}
             required
           />
         </FormControl>
         <FormControl mb={4}>
           <FormLabel>Date de début</FormLabel>
           <Input
-            value={data.start_date || ''}
-            onChange={handleInputChange('start_date')}
+            name="start_date"
             type="date"
             min={strftime(DATE_FORMAT)}
+            ref={register}
             required
           />
         </FormControl>
         <FormControl mb={4}>
           <FormLabel>Date de fin</FormLabel>
-          <Input
-            value={data.end_date || ''}
-            onChange={handleInputChange('end_date')}
-            type="date"
-            min={strftime(DATE_FORMAT)}
-            required
-          />
+          <Input name="end_date" type="date" min={watching} ref={register} required />
         </FormControl>
         <FormControl>
           <FormLabel>Informations</FormLabel>
-          <Textarea
-            value={data.info || ''}
-            onChange={handleInputChange('info')}
-            placeholder="Tarifs, lieu, horaires..."
-          />
+          <Textarea name="info" placeholder="Tarifs, lieu, horaires..." ref={register} required />
         </FormControl>
       </ModalBody>
       <ModalFooter>
         <Button variant="ghost" mr={2} onClick={onClose}>
           Annuler
         </Button>
-        <Button colorScheme="blue" onClick={handleSubmit} isLoading={submitting}>
+        <Button colorScheme="blue" onClick={handleSubmit(onSubmit)} isLoading={submitting}>
           {edit ? 'Éditer' : 'Ajouter'}
         </Button>
       </ModalFooter>
