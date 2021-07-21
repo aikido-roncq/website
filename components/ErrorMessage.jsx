98b6bb8c3f6ceaf4e859e59f8ca46294186b4dae
Ludal <ludovic.chombeau@gmail.com>
2021-07-21 08:48:45 +0200

    refactor: handle AddEvent form errors

---
+++ components/ErrorMessage.jsx
@@ -0,0 +1,15 @@
+const { WarningTwoIcon } = require('@chakra-ui/icons');
+
+const ErrorMessage = ({ error }) => {
+  if (!error) {
+    return null;
+  }
+
+  return (
+    <small style={{ color: 'red' }}>
+      <WarningTwoIcon /> {error.message}
+    </small>
+  );
+};
+
+export default ErrorMessage;
