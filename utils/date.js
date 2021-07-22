7b7fce55529388c70cb39c0fc2d07a45b46b05c5
Ludal <ludovic.chombeau@gmail.com>
2021-07-22 13:59:48 +0200

    feat: add relativeDateString function

--- utils/date.js
+++ utils/date.js
@@ -18,3 +18,41 @@ export const formatDateRange = (startStr, endStr) => {
 };
 
 export const formatDate = date => new Date(date).toLocaleDateString();
+
+/**
+ * Handle the plural of the singular word passed as a parameter depending on the count
+ * @param {Number} count the count
+ * @param {String} singular the singular word
+ * @returns {String} the singular or plural word, depending on the count
+ */
+const handlePlural = (count, singular) => (count === 1 ? singular : singular + 's');
+
+/**
+ * Get the relative date string of a date from today
+ * @param {Date} date the date
+ * @returns {string} the relative date string
+ */
+export const relativeDateString = date => {
+  const today = new Date();
+  const diff = today.getTime() - date.getTime();
+  const minute = 1000 * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  if (diff < minute) {
+    return `à l'instant`;
+  } else if (diff < hour) {
+    const minutes = Math.floor(diff / minute);
+    return `il y a ${minutes} ${handlePlural(minutes, 'minute')}`;
+  } else if (diff < day) {
+    const hours = Math.floor(diff / hour);
+    return `il y a ${hours} ${handlePlural(hours, 'heure')}`;
+  } else if (diff < day * 2) {
+    return `hier`;
+  } else if (diff < day * 7) {
+    const days = Math.floor(diff / day);
+    return `il y a ${days} jours`;
+  } else {
+    return `le ${formatDate(date)}`;
+  }
+};
