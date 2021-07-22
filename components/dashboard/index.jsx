06bea22d603da0b399b1977648c984314d08c7e0
Ludal <ludovic.chombeau@gmail.com>
2021-07-22 09:36:48 +0200

    feat: add query params for dashboard tabs

--- components/dashboard/index.jsx
+++ components/dashboard/index.jsx
@@ -3,29 +3,62 @@ import Articles from './Articles';
 import Events from './Events';
 import Header from './Header';
 import { Box, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
-const Dashboard = () => (
-  <>
-    <Head title="Tableau de bord" />
+const TABS = [
+  {
+    key: 'articles',
+    label: 'Articles',
+    component: <Articles />,
+  },
+  {
+    key: 'events',
+    label: 'Événements',
+    component: <Events />,
+  },
+];
 
-    <Box p={5}>
-      <Header />
-      <Tabs variant="soft-rounded" align="center">
-        <TabList>
-          <Tab>Articles</Tab>
-          <Tab>Événements</Tab>
-        </TabList>
-        <TabPanels>
-          <TabPanel>
-            <Articles />
-          </TabPanel>
-          <TabPanel>
-            <Events />
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
-    </Box>
-  </>
-);
+const Dashboard = () => {
+  const router = useRouter();
+  const [tabIndex, setTabIndex] = useState(0);
+
+  useEffect(() => {
+    const { tab } = router.query;
+    const index = TABS.findIndex(t => t.key === tab);
+    if (index !== -1) {
+      setTabIndex(index);
+    }
+  }, []);
+
+  useEffect(() => {
+    router.push({
+      pathname: router.pathname,
+      query: { tab: TABS[tabIndex].key },
+    });
+  }, [tabIndex]);
+
+  return (
+    <>
+      <Head title="Tableau de bord" />
+
+      <Box p={5}>
+        <Header />
+        <Tabs variant="soft-rounded" align="center" index={tabIndex} onChange={setTabIndex}>
+          <TabList>
+            {TABS.map(tab => (
+              <Tab key={tab.key}>{tab.label}</Tab>
+            ))}
+          </TabList>
+          <TabPanels>
+            {TABS.map(tab => (
+              <TabPanel key={tab.key}>{tab.component}</TabPanel>
+            ))}
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </>
+  );
+};
 
 export default Dashboard;
